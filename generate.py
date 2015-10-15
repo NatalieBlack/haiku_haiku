7fbe5fd0cff35e0c21c42773f8ed93e53028cf50
NatalieBlack <natalievictoriablack@gmail.com>
2015-10-15 11:55:44 -0400

    it's a haikugitst!

--- generate.py
+++ generate.py
@@ -1,58 +1,100 @@
 from nltk.parse.generate import generate
-from nltk import CFG
-from nltk.grammar import Nonterminal
-import random
-import re
-seven = ['Arteriosclerosis' , 'Artificiality' , 'Autobiographical' , 'Conceptualization' , 'Decriminalization' , 'Disproportionality' , 'Editorializing' , 'Heterogeneity' , 'Infinitesimally' , 'Intercolonization' , 'Irrefutability' , 'Irreversibility' , 'Manoeuvrability' , 'Megalomaniacal' , 'Oversimplification' , 'Proletarianism' , 'Sentimentalization' , 'Superficiality']
-six = ['Antediluvian' , 'Circumnavigation' , 'Disambiguation' , 'Disappriciated' , 'Discontinuity' , 'Dissimilation' , 'Eleemosynary' , 'Incomprehensible' , 'Indefatigable' , 'Prestidigitation' , 'Responsibility' , 'Sesquicentennial' , 'Superannuated' , 'Supernumerary' , 'Teleportality' , 'Verisimilitude']
-five = ['Assimilation' , 'Conscientiousness' , 'Creativity' , 'Diagnostician' , 'Electricity' , 'Humiliation' , 'Mathematical' , 'Opportunity' , 'Popularity' , 'Similarity' , 'Incredulity' , 'Paediatrician' , 'Perpendicular' , 'Unbelievable' , 'University' , 'Vocabulary']
-four = ['Bureaucratic' , 'Contradicting' , 'Copulation', 'Degenerate', 'Dictionary' , 'Directory' , 'Disestablish', 'Execution' , 'Indecisive', 'Insoluble' , 'Mandatory', 'Obligation' , 'Obviously' , 'Overwhelming' , 'Persecution' , 'Population' , 'Similitude' , 'Supposedly' , 'Television' , 'Termination' , 'Undoubtedly']
-three = ['Adequate', 'Amazement', 'Attention', 'Attractive', 'Average', 'Banana', 'Bicycle' , 'Blindingly', 'Buffalo' , 'Cabinet', 'Certainly', 'Companion', 'Complement', 'Conference', 'Conference' , 'Connection', 'Considered', 'Curious' , 'Customary' , 'Dangerous' , 'Difficult', 'Dilemma', 'Dinosaur' , 'Documents', 'Easily', 'Electric' , 'Everything', 'Exciting' , 'Exercise' , 'Exhaustion' , 'Family' , 'Favourite' , 'Glacier', 'Happenings', 'Horizon' , 'Illustrate', 'Industry' , 'Instrument', 'Legacy', 'Liberal', 'Library' , 'Numerous' , 'Palatial', 'Period' , 'Persistent' , 'Photograph', 'Reunion' , 'Substitute' , 'Terrible' , 'Typical' ]
-two = ['Acquired', 'Again' , 'Aircrafts', 'Alleged', 'Ancient', 'Auto', 'Avoid', 'Bacon', 'Basket', 'Behaved', 'Between', 'Bible', 'Billion', 'Birthright', 'Brilliant', 'Buildings', 'Collapsed', 'Conscience', 'Cycle' , 'Danger', 'Daring', 'Discuss', 'Drearier', 'Enter', 'Evening', 'Every', 'Evil', 'Forceps', 'Hamper', 'Hockey', 'Hundred', 'Inspired', 'Involved', 'Kitten', 'London', 'Lumber', 'Mountain', 'Photo', 'Playful', 'Police', 'Sheepish']
-one = ['A', 'All', 'And', 'Are', 'As', 'Be', 'By', 'Day', 'Did', 'Each', 'Few', 'For', 'Get', 'Have', 'He', 'Him', 'His', 'I', 'In', 'Is', 'It', 'Long', 'Man', 'Me', 'More', 'Much', 'My', 'New', 'Not', 'Now', 'Of', 'Off', 'Old', 'On', 'One', 'Or', 'Out', 'Same', 'She', 'So', 'State', 'Than', 'That', 'The', 'They', 'Time', 'To', 'Up', 'War', 'Was']
+from nltk import CFG as G
+from nltk.grammar import Nonterminal as N
+import random as a_random
+seven = ['arteriosclerosis' , 'artificiality' , 'autobiographical' , 'conceptualization' , 'decriminalization' , 'disproportionality' , 'editorializing' , 'heterogeneity' , 'infinitesimally' , 'intercolonization' , 'irrefutability' , 'irreversibility' , 'manoeuvrability' , 'megalomaniacal' , 'oversimplification' , 'proletarianism' , 'sentimentalization' , 'superficiality']
+six = ['antediluvian' , 'circumnavigation' , 'disambiguation' , 'disappriciated' , 'discontinuity' , 'dissimilation' , 'eleemosynary' , 'incomprehensible' , 'indefatigable' , 'prestidigitation' , 'responsibility' , 'sesquicentennial' , 'superannuated' , 'supernumerary' , 'teleportality' , 'verisimilitude']
+five = ['assimilation' , 'conscientiousness' , 'creativity' , 'diagnostician' , 'electricity' , 'humiliation' , 'mathematical' , 'opportunity' , 'popularity' , 'similarity' , 'incredulity' , 'paediatrician' , 'perpendicular' , 'unbelievable' , 'university' , 'vocabulary']
+four = ['bureaucratic' , 'contradicting' , 'copulation', 'degenerate', 'dictionary' , 'directory' , 'disestablish', 'execution' , 'indecisive', 'insoluble' , 'mandatory', 'obligation' , 'obviously' , 'overwhelming' , 'persecution' , 'population' , 'similitude' , 'supposedly' , 'television' , 'termination' , 'undoubtedly']
+three = ['adequate', 'amazement', 'attention', 'attractive', 'average', 'banana', 'bicycle' , 'blindingly', 'buffalo' , 'cabinet', 'certainly', 'companion', 'complement', 'conference', 'conference' , 'connection', 'considered', 'curious' , 'customary' , 'dangerous' , 'difficult', 'dilemma', 'dinosaur' , 'documents', 'easily', 'electric' , 'everything', 'exciting' , 'exercise' , 'exhaustion' , 'family' , 'favourite' , 'glacier', 'happenings', 'horizon' , 'illustrate', 'industry' , 'instrument', 'legacy', 'liberal', 'library' , 'numerous' , 'palatial', 'period' , 'persistent' , 'photograph', 'reunion' , 'substitute' , 'terrible' , 'typical' ]
+two = ['acquired', 'again' , 'aircrafts', 'alleged', 'ancient', 'auto', 'avoid', 'bacon', 'basket', 'behaved', 'between', 'bible', 'billion', 'birthright', 'brilliant', 'buildings', 'collapsed', 'conscience', 'cycle' , 'danger', 'daring', 'discuss', 'drearier', 'enter', 'evening', 'every', 'evil', 'forceps', 'hamper', 'hockey', 'hundred', 'inspired', 'involved', 'kitten', 'london', 'lumber', 'mountain', 'photo', 'playful', 'police', 'sheepish']
+one = ['a', 'all', 'and', 'are', 'as', 'be', 'by', 'day', 'did', 'each', 'few', 'for', 'get', 'have', 'he', 'him', 'his', 'i', 'in', 'is', 'it', 'long', 'man', 'me', 'more', 'much', 'my', 'new', 'not', 'now', 'of', 'off', 'old', 'on', 'one', 'or', 'out', 'same', 'she', 'so', 'state', 'than', 'that', 'the', 'they', 'time', 'to', 'up', 'war', 'was']
 other_one = ['not', 'blue','good','day','night','hour','old','long','small','time','moon','sun','earth','fire','air','quite','go','years','same','facts','love']
-grammar = """haiku -> five seven five 
-      seven -> 'Arteriosclerosis' | 'Artificiality' | 'Autobiographical' | 'Conceptualization' | 'Decriminalization' | 'Disproportionality' | 'Editorializing' | 'Heterogeneity' | 'Infinitesimally' | 'Intercolonization' | 'Irrefutability' | 'Irreversibility' | 'Manoeuvrability' | 'Megalomaniacal' | 'Oversimplification' | 'Proletarianism' | 'Sentimentalization' | 'Superficiality'
-      seven -> one six
-      seven -> two five 
-      seven -> three four 
-      seven -> four three
-      seven -> five two
-      seven -> six one
-      six -> 'Antediluvian' | 'Circumnavigation' | 'Disambiguation' | 'Disappriciated' | 'Discontinuity' | 'Dissimilation' | 'Eleemosynary' | 'Incomprehensible' | 'Indefatigable' | 'Prestidigitation' | 'Responsibility' | 'Sesquicentennial' | 'Superannuated' | 'Supernumerary' | 'Teleportality' | 'Verisimilitude'
+this_is_the_grammar = """haiku -> five sev five 
+      sev -> 'arteriosclerosis' | 'artificiality' | 'autobiographical' | 'conceptualization' | 'decriminalization' | 'disproportionality' | 'editorializing' | 'heterogeneity' | 'infinitesimally' | 'intercolonization' | 'irrefutability' | 'irreversibility' | 'manoeuvrability' | 'megalomaniacal' | 'oversimplification' | 'proletarianism' | 'sentimentalization' | 'superficiality'
+      sev -> one six
+      sev -> two five 
+      sev -> three four 
+      sev -> four three
+      sev -> five two
+      sev -> six one
+      six -> 'antediluvian' | 'circumnavigation' | 'disambiguation' | 'disappriciated' | 'discontinuity' | 'dissimilation' | 'eleemosynary' | 'incomprehensible' | 'indefatigable' | 'prestidigitation' | 'responsibility' | 'sesquicentennial' | 'superannuated' | 'supernumerary' | 'teleportality' | 'verisimilitude'
       six -> one five
       six -> two four
       six -> three three
       six -> four two
       six -> five one
-      five -> 'Assimilation' | 'Conscientiousness' | 'Creativity' | 'Diagnostician' | 'Electricity' | 'Humiliation' | 'Mathematical' | 'Opportunity' | 'Popularity' | 'Similarity' | 'Incredulity' | 'Paediatrician' | 'Perpendicular' | 'Unbelievable' | 'University' | 'Vocabulary'
+      five -> 'assimilation' | 'conscientiousness' | 'creativity' | 'diagnostician' | 'electricity' | 'humiliation' | 'mathematical' | 'opportunity' | 'popularity' | 'similarity' | 'incredulity' | 'paediatrician' | 'perpendicular' | 'unbelievable' | 'university' | 'vocabulary'
       five -> two three
       five -> three two
-      four -> 'Bureaucratic' | 'Contradicting' | 'Copulation'| 'Degenerate'| 'Dictionary' | 'Directory' | 'Disestablish'| 'Execution' | 'Indecisive'| 'Insoluble' | 'Mandatory'| 'Obligation' | 'Obviously' | 'Overwhelming' | 'Persecution' | 'Population' | 'Similitude' | 'Supposedly' | 'Television' | 'Termination' | 'Undoubtedly'
+      four -> 'bureaucratic' | 'contradicting' | 'copulation'| 'degenerate'| 'dictionary' | 'directory' | 'disestablish'| 'execution' | 'indecisive'| 'insoluble' | 'mandatory'| 'obligation' | 'obviously' | 'overwhelming' | 'persecution' | 'population' | 'similitude' | 'supposedly' | 'television' | 'termination' | 'undoubtedly'
       four -> two two
       four -> one three 
       four -> three one
-      three -> 'Adequate'| 'Amazement'| 'Attention'| 'Attractive'| 'Average'| 'Banana'| 'Bicycle' | 'Blindingly'| 'Buffalo' | 'Cabinet'| 'Certainly'| 'Companion'| 'Complement'| 'Conference'| 'Conference' | 'Connection'| 'Considered'| 'Curious' | 'Customary' | 'Dangerous' | 'Difficult'| 'Dilemma'| 'Dinosaur' | 'Documents'| 'Easily'| 'Electric' | 'Everything'| 'Exciting' | 'Exercise' | 'Exhaustion' | 'Family' | 'Favourite' | 'Glacier'| 'Happenings'| 'Horizon' | 'Illustrate'| 'Industry' | 'Instrument'| 'Legacy'| 'Liberal'| 'Library' | 'Numerous' | 'Palatial'| 'Period' | 'Persistent' | 'Photograph'| 'Reunion' | 'Substitute' | 'Terrible' | 'Typical' 
-      two -> 'Acquired'| 'Again' | 'Aircrafts'| 'Alleged'| 'Ancient'| 'Auto'| 'Avoid'| 'Basket'| 'Behaved'| 'Between'| 'Bible'| 'Billion'| 'Birthright'| 'Brilliant'| 'Buildings'| 'Collapsed'| 'Conscience'| 'Cycle' | 'Danger'| 'Daring'| 'Discuss'| 'Drearier'| 'Enter'| 'Evening'| 'Every'| 'Evil'| 'Forceps'| 'Hamper'| 'Hockey'| 'Hundred'| 'Inspired'| 'Involved'| 'Kitten'| 'London'| 'Lumber'| 'Mountain'| 'Photo'| 'Playful'| 'Police'| 'Sheepish'
-      one -> '""" + random.choice(other_one) + """'
-      one -> '""" + random.choice(other_one) + """'
-      one -> '""" + random.choice(other_one) + """'
-      one -> '""" + random.choice(other_one) + "'"
-
-g = CFG.fromstring(grammar)
-#for poem in generate(g, n=100):
-#    print ' '.join(poem)
-
-fives = []
+      three -> 'adequate'| 'amazement'| 'attention'| 'attractive'| 'average'| 'banana'| 'bicycle' | 'blindingly'| 'buffalo' | 'cabinet'| 'certainly'| 'companion'| 'complement'| 'conference'| 'conference' | 'connection'| 'considered'| 'curious' | 'customary' | 'dangerous' | 'difficult'| 'dilemma'| 'dinosaur' | 'documents'| 'easily'| 'electric' | 'everything'| 'exciting' | 'exercise' | 'exhaustion' | 'family' | 'favourite' | 'glacier'| 'happenings'| 'horizon' | 'illustrate'| 'industry' | 'instrument'| 'legacy'| 'liberal'| 'library' | 'numerous' | 'palatial'| 'period' | 'persistent' | 'photograph'| 'reunion' | 'substitute' | 'terrible' | 'typical' 
+      two -> 'acquired'| 'again' | 'aircrafts'| 'alleged'| 'ancient'| 'auto'| 'avoid'| 'basket'| 'behaved'| 'between'| 'bible'| 'billion'| 'birthright'| 'brilliant'| 'buildings'| 'collapsed'| 'conscience'| 'cycle' | 'danger'| 'daring'| 'discuss'| 'drearier'| 'enter'| 'evening'| 'every'| 'evil'| 'forceps'| 'hamper'| 'hockey'| 'hundred'| 'inspired'| 'involved'| 'kitten'| 'london'| 'lumber'| 'mountain'| 'photo'| 'playful'| 'police'| 'sheepish'
+      one -> '""" + a_random.choice(other_one) + """'
+      one -> '""" + a_random.choice(other_one) + """'
+      one -> '""" + a_random.choice(other_one) + """'
+      one -> '""" + a_random.choice(other_one) + "'"
+pentas = []
 sevens = []
-for poem in generate(g, start=Nonterminal('five')):
-  fives.append(' '.join(poem))
+with_blank_spaces = ' '
 
-for poem in generate(g, start=Nonterminal('seven')):
-  sevens.append(' '.join(poem))
 
-line1 = random.choice(fives)
-line2 = random.choice(sevens)
-line3 = random.choice(fives)
+############################################
+############################################
+############################################
+def choose_line(some_lines):#5
+    return a_random.choice(#7
+                    some_lines).lower() #5
 
-print '\n'.join([line1,line2,line3]).lower()
+############################################
+
+############################################
+choose = choose_line #5
+
+g = G.fromstring(#7
+                    this_is_the_grammar) #5
+############################################
+
+############################################
+while not len(pentas):#5
+    for poem in generate(g, #7
+                           start=N('five')): #5
+############################################
+
+############################################
+      pentas.append(#5
+                    with_blank_spaces.join(poem))#7
+
+fives = pentas #5
+############################################
+
+############################################
+third = choose(fives) #5
+first = choose(fives) #7
+
+def display_the(poem):#5
+############################################
+    print '\n'.join(poem)#5
+
+for poem in generate(g,#7
+                       start=N('sev')): #5
+############################################
+  sevens.append(#5
+                with_blank_spaces.join(poem))#7
+
+
+sept = sevens#5
+############################################
+ 
+
+
+############################################
+mid = choose(sept)#5
+haiku = [first,mid,third]#7
+
+display_the(haiku)#5
+
+############################################
